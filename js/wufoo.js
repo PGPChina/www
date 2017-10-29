8f1111ff86d73589be5c0fa8f7d8361b76fc6d68
Steve Strassmann <steve@strassmann.com>
2017-10-29 10:41:32 +0800

    fix form

--- js/wufoo.js
+++ js/wufoo.js
@@ -1,21 +1,17 @@
-var ztz4hk90hfxf28;
 
-(function(d, t) {
-    var s = d.createElement(t),
-	options = {
-	    'userName':'matinee',
-	    'formHash':'ztz4hk90hfxf28',
-	    'autoResize':true,
-	    'height':'603',
-	    'async':true,
-	    'host':'wufoo.com',
-	    'header':'show',
-	    'ssl':true};
-    s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
-    s.onload = s.onreadystatechange = function() {
-	var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
-	try { ztz4hk90hfxf28 = new WufooForm();ztz4hk90hfxf28.initialize(options);ztz4hk90hfxf28.display(); } catch (e) {}};
-    var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
-    $('input.submit').addClass('btn btn-primary');
+var ztz4hk90hfxf28;(function(d, t) {
+var s = d.createElement(t), options = {
+'userName':'pgpchina',
+'formHash':'ztz4hk90hfxf28',
+'autoResize':true,
+'height':'320',
+'async':true,
+'host':'wufoo.com',
+'header':'show',
+'ssl':true};
+s.src = ('https:' == d.location.protocol ? 'https://' : 'http://') + 'www.wufoo.com/scripts/embed/form.js';
+s.onload = s.onreadystatechange = function() {
+var rs = this.readyState; if (rs) if (rs != 'complete') if (rs != 'loaded') return;
+try { ztz4hk90hfxf28 = new WufooForm();ztz4hk90hfxf28.initialize(options);ztz4hk90hfxf28.display(); } catch (e) {}};
+var scr = d.getElementsByTagName(t)[0], par = scr.parentNode; par.insertBefore(s, scr);
 })(document, 'script');
-
